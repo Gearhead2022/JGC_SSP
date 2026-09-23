8819d8b453fe4987d091d3b3f82924c5f7e402c8
mozby69 <mozbygreen13@gmail.com>
2026-09-23 11:38:26 +0800

    add

--- apps/web/src/constants/route.constants.ts
+++ apps/web/src/constants/route.constants.ts
@@ -11,10 +11,7 @@ export const ROUTES = {
     SSP: {
         COMPSLIP: "/ssp/comp-slip",
         COLLECTION: "/ssp/loan-collection",
-<<<<<<< HEAD
-        PENSIONER:"/pensioner/pensioner_list"
-=======
+        PENSIONER:"/pensioner/pensioner_list",
         SUPPLEMENTARY: "/ssp/supplementary-collection",
->>>>>>> 175bdfda472426dda3e630387078fcb96cb269c9
     },
 } as const;
